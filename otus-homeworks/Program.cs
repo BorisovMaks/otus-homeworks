7d4887f11a328a50a3ff461cc52f37e8f9aa2b9e
Borisov Maks <BorisovMaks@users.noreply.github.com>
2024-12-01 20:56:51 +0300

    Add project files.

---
+++ otus-homeworks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace otus_homeworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
